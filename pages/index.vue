5958407bfcf7bdf584692ab8d4cf618d6c1c9673
Adam Miedema <armgitaar@yahoo.com>
2021-01-12 13:18:22 -0700

    update links

--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,7 @@
     <div class="p-4 w-full">
       <div class="grid grid-cols-10 gap-4">
         <div class="col-span-12 sm:col-span-6 md:col-span-3">
-          <a href="https://yahoo.com" target="_blank" class="flex flex-row shadow-sm bg-blue-100 hover:bg-blue-200 rounded p-4 items-center">
+          <a href="https://docstemplate.cleavr.cloud" target="_blank" class="flex flex-row shadow-sm bg-blue-100 hover:bg-blue-200 rounded p-4 items-center">
             <div class="flex items-center justify-center flex-shrink-0 h-12 w-12 rounded-xl text-blue-500">
               <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
@@ -18,7 +18,7 @@
           </a>
         </div>
         <div class="col-span-12 sm:col-span-6 md:col-span-3">
-          <a href="https://yahoo.com" target="_blank" class="flex flex-row shadow-sm bg-green-100 hover:bg-green-200 rounded p-4 items-center">
+          <a href="https://github.com/cleavr/docs-template" target="_blank" class="flex flex-row shadow-sm bg-green-100 hover:bg-green-200 rounded p-4 items-center">
             <div class="flex items-center justify-center flex-shrink-0 h-12 w-12 rounded-xl text-green-500">
               <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
